274dc27b27e0b7ce344f11cbbb98f985498774be
Sebastian <seba.laca@gmail.com>
2022-10-02 20:31:21 +0200

    Update proyect

---
+++ js/app.js
@@ -0,0 +1,28 @@
+//este tipo de import importa todos los modulos de interfaz en una sola linea
+import * as UI from './interfaz.js'
+
+UI.formularioBuscar.addEventListener('submit', buscarCancion)
+
+
+function buscarCancion(e) {
+    e.preventDefault()
+
+    //Obtener datos del formulario
+    const artista = document.querySelector('#artista').value
+    const cancion = document.querySelector('#cancion').value
+
+    if (artista === '' || cancion === '') {
+        //El usuario dejo un campo vacio, mostar error
+        UI.divMensajes.textContent = 'Error todos los campos son obligatorios'
+        UI.divMensajes.classList.add('error')
+
+        setTimeout(() => {
+            UI.divMensajes.textContent = ''
+            UI.divMensajes.classList.remove('error')
+        }, 3000);
+
+        return
+    }
+
+    //Consultar API
+}
